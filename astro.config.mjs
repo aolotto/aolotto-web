7a36cf308a9360226c589cbc700b719a06b230e4
Brickhu <brickhu@126.com>
2024-07-22 23:50:17 +0800

    update homepage

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ export default defineConfig({
     defaultLocale: "en",
     locales: ["en", "cn"],
     routing: {
-      prefixDefaultLocale: true,
+      prefixDefaultLocale: false,
       redirectToDefaultLocale: true
     },
     fallback: {
